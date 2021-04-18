b38d89926520800632e276f33ed89bcb642a2637
Lewin Villar <lewin.villar@gmail.com>
2021-04-18 03:09:10 +0000

    Before crash

--- citz/citz/public_methods.py
+++ citz/citz/public_methods.py
@@ -17,7 +17,7 @@ def get_events(date, responsible):
 def branches():
     branches = frappe.db.sql("SELECT name FROM `tabBranch`", as_dict=1)
     for branch in branches: 
-        branch['schedule'] = frappe.db.sql("SELECT name, opening_time, closing_time, is_open FROM `tabBranch Schedule` WHERE parent = %(parent)s", {"parent": "castellana"}, as_dict=1)
+        branch['schedule'] = frappe.db.sql("SELECT day_of_week as name, DATE_FORMAT(opening_time, '%%H:%%i:%%S') as opening_time, DATE_FORMAT(closing_time, '%%H:%%i:%%S') as closing_time, is_open FROM `tabBranch Schedule` WHERE parent = %(parent)s", {"parent": "castellana"}, as_dict=1)
     return branches
 
 @frappe.whitelist()
@@ -48,3 +48,8 @@ def event(subject, responsible, starts_on, ends_on, description, branch):
     newEvent.save(ignore_permissions=True)
     newEvent.reload()
     return newEvent
+
+@frappe.whitelist()
+def new_customer():
+    customer = frappe.new_doc("Customer")
+    return customer; 
---
+++ citz/public/js/event.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and Contributors
+// MIT License. See license.txt
+frappe.provide("frappe.desk");
+
+frappe.ui.form.on("Event", {
+	refresh: function(frm) {
+		if(frm.doc.event_participants) {
+			let hasCustomer = frm.doc.event_participants
+				.findIndex(value => value.reference_doctype == "Customer")
+			if (hasCustomer > -1) {
+				frm.add_custom_button(__('Create Sales Order'), function() {
+					frappe.new_doc("Sales Order")
+				})
+			}
+		}
+
+		frm.remove_custom_button(__('Add Contacts'), __("Add Participants"));
+		frm.remove_custom_button(__('Add Leads'), __("Add Participants"));
+		frm.remove_custom_button(__('Add Customers'), __("Add Participants"));
+		frm.remove_custom_button(__('Add Suppliers'), __("Add Participants"));
+		frm.remove_custom_button(__('Add Employees'), __("Add Participants"));
+		frm.remove_custom_button(__('Add Sales Partners'), __("Add Participants"));
+
+		frm.add_custom_button(__('Add Customer'), function () {
+			new frappe.desk.eventParticipants(frm, "Customer");
+		})
+
+		frm.custom_buttons[__('Add Customer')].addClass("btn-primary")
+	}
+});
